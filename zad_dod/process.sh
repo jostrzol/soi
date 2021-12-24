fd4fb31fbf456baaf925c6cc92e106361bf05c22
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2021-12-24 16:44:13 +0100

    process bash-compatible

--- zad_dod/process.sh
+++ zad_dod/process.sh
@@ -1,9 +1,9 @@
 #!/bin/env zsh
 
-time=$(date +%T.%N)
-echo "[${time[1, 12]}] ($$) processing file $1..." >&2
+time=$(date +%T.%N | cut -c 1-12)
+echo "[$time] ($$) processing file $1..." >&2
 
 sleep $((RANDOM % 4 + 1))
 
-time=$(date +%T.%N)
-echo "[${time[1, 12]}] ($$) file $1 processed" >&2
+time=$(date +%T.%N | cut -c 1-12)
+echo "[$time] ($$) file $1 processed" >&2
