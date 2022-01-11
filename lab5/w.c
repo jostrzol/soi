f166065f4ded0fb7d6b9717d55033fa5efee4017
Ogurczak <mr.oguraskowiec@gmail.com>
2022-01-11 16:39:50 +0100

    Add files via upload

---
+++ lab5/w.c
@@ -0,0 +1,22 @@
+/* w.c - polecenie w przyjmuje jako argument 1 albo 0 */
+/* wlaczajac/wylaczajac algorytm worst fit w systemie Minix */
+#include <stdlib.h>
+#include <unistd.h>
+#include <lib.h>
+
+
+PUBLIC int worst_fit( int w )
+{
+    message msg;
+    msg.m_u.m_m1.m1i1 = w;
+    return _syscall(MM, WORST_FIT, &msg);
+}
+
+int
+main( int argc, char *argv[] )
+{
+	if( argc < 2 )
+		return 1;
+	worst_fit( atoi( argv[1] ) );
+	return 0;
+}
