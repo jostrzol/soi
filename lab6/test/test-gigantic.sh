bf40af500dd86f996486d47482ed0f790f93d63b
Jakub Ostrzołek <01158865@pw.edu.pl>
2022-01-19 17:19:26 +0100

    add gigantic file generation to test-gigantic

--- lab6/test/test-gigantic.sh
+++ lab6/test/test-gigantic.sh
@@ -5,6 +5,9 @@ fs_file="test.myfs"
 
 $cmd create $fs_file
 
+# make 10235 * 1016 B file (that fills 10235 datablocks) 
+truncate -s 10398760 gigantic
+
 $cmd cp-to $fs_file gigantic gigantic
 # 5 datablocks should be left
 
